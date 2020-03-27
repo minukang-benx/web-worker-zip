21fb66ec42483ec33d135c70e031dd3c88b32ab9
MinuKang <minukang@bighitcorp.com>
2020-03-27 13:57:52 +0900

    change axios version

--- src/zip.worker.js
+++ src/zip.worker.js
@@ -1,58 +1,40 @@
-import "whatwg-fetch";
-import "web-streams-polyfill";
-import TextEncoder from "text-encoding";
+import axios from "axios";
 import JSZip from "jszip";
-import fetchReadableStream from "fetch-readablestream";
-
-global.TextEncoder = TextEncoder;
 
 const ctx = self;
-
-function delay (ms) {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms);
-  });
-}
 
 ctx.addEventListener("message", async (event) => {
   const files = event.data.files;
   if (files && files.length) {
     ctx.postMessage({ type: "start" });
-    const collection = await Promise.all(files.map(async file => {
-      const response = await fetchReadableStream(file);
+    const progressResult = new Array(files.length);
+    const collection = await Promise.all(files.map(async (file, index) => {
+      const { data: response } = await axios({
+        url: file,
+        responseType: "blob",
+        onDownloadProgress({ loaded, total }) {
+          progressResult[index] = loaded / total;
+          const totalProgress = progressResult.reduce((result, value) => result + (value || 0), 0);
+          ctx.postMessage({
+            type: "progress",
+            data: {
+              value: totalProgress / files.length
+            }
+          });
+        }
+      });
       return {
         response,
         file,
-        size: Number(response.headers.get('Content-Length'))
       };
     }));
-    const totalSize = collection.reduce((result, { size }) => {
-      return result + size;
-    }, 0);
-    let receivedSize = 0;
     const blobs = await Promise.all(collection.map(async ({ file, response }) => {
-      const reader = response.body.getReader();
-      const chunks = [];
-      while (true) {
-        const { done, value } = await reader.read();
-        if (done) {
-          break;
-        }
-        chunks.push(value);
-        receivedSize += value.length;
-        ctx.postMessage({
-          type: "progress",
-          data: {
-            value: receivedSize / totalSize
-          }
-        });
-        await delay(150);
-      }
       return {
         file,
-        blob: new Blob(chunks)
+        blob: response
       }
     }));
+    console.log("End Progress", blobs);
     ctx.postMessage({ type: "end-progress" });
     const zip = new JSZip();
     blobs.forEach(({ file, blob }) => {
